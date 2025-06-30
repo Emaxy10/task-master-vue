85d39745034db8962b38a359550a0902fa42c001
emaxy10 <emmanuelchigozie01@gmail.com>
2025-06-30 23:36:36 +0100

    30/6/25

--- vue.config.js
+++ vue.config.js
@@ -1,4 +1,8 @@
 const { defineConfig } = require('@vue/cli-service')
 module.exports = defineConfig({
-  transpileDependencies: true
+  transpileDependencies: true,
+   devServer: {
+    host: 'localhost',  // You can use '0.0.0.0' to expose it on your network
+    port: 3000           // Change this to your desired port
+  }
 })
